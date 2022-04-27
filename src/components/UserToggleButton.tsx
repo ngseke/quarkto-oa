915c012814776fa195a012970c8e18adfb27a18f
Huang Xingqiao <a92304a92304@gmail.com>
2022-04-27 17:36:09 +0800

    feat: create component UserToggleButton

---
+++ src/components/UserToggleButton.tsx
@@ -0,0 +1,12 @@
+export default function UserToggleButton () {
+  return (
+    <button
+      className="
+        inline-block text-base leading-[30px] px-[30px] py-[14px]
+        border-[1px] border-black
+        after:content-['▼'] after:ml-2
+        rounded-full
+      "
+    >UserName</button>
+  )
+}
