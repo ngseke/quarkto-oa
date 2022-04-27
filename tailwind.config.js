3a133e7536d197b5876c0201c2ee081bf80634c2
Huang Xingqiao <a92304a92304@gmail.com>
2022-04-27 17:36:41 +0800

    chore: update tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+    },
+    colors: {
+      transparent: 'transparent',
+      orange: '#FB9000',
+      black: '#1D1D1D',
+    },
     extend: {},
   },
   plugins: [],
