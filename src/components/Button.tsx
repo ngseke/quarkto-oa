80bd5dd6d8e03e89abecfc0055a1e9bcde6993ed
Huang Xingqiao <a92304a92304@gmail.com>
2022-04-28 13:59:51 +0800

    fix: add attribute `type`

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,6 +10,7 @@ type ButtonType =
 export default function Button ({ secondary, block, ...restProps }: ButtonType) {
   return (
     <button
+      type="button"
       className={clsx(
         'h-[48px] px-[30px] rounded-full border-[1px]',
         secondary ? 'border-black hover:bg-white' : 'bg-black text-white border-transparent',
